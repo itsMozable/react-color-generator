517e8eff96616d1294f6b3e85e6c8ab6def998fc
tattoomozi <tattoomozi@icloud.com>
2023-05-03 20:13:55 +0200

    First Draft

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
+// Must have in React
+export default function App() {
+  // Button Action for changable Color
+  const [poopy, setPoopy] = useState(randomColor());
+  return (
+    <div className="App">
+      <h1>Color Generator </h1>
+      <div>
+        {poopy}
+        {/* Add a Button with on-click-action */}
+        <button
+          onClick={() => {
+            const otherColor = randomColor();
+            setPoopy(otherColor);
+          }}
+        >
+          Generate
+        </button>
+        {/* Create Box and display code and color */}
+        <div style={{ backgroundColor: poopy }}>
+          <h1>Generated Color: {poopy}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
